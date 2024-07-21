af95860335e3412c0ab0da815018ee8065db0575
khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2024-07-21 19:57:41 +0100

    Les slots

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -4,15 +4,18 @@ export default class CustomButton extends HTMLElement {
         this.shadow = this.attachShadow({mode:'open'})
         this.shadow.innerHTML = `
         <style>
+            :host{
+                --bg-gray: #000;
+            }
             button{
                 border: none;
                 border-radius: 0.325rem;
                 padding: 0.5rem 1.125rem;
-                background: #333;
+                background: var(--bg-gray);
                 color: #fff;
             }
         </style>
-        <button>MonTexte</button</button>
+        <button><slot/></button>
         `
     }
 }
