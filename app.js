8601a68c83d11f61061a2d5b6a2ec55ce9b8327f
khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2024-07-21 18:37:19 +0100

    Init project

---
+++ app.js
@@ -0,0 +1,18 @@
+class SecongTimer extends HTMLElement{
+    constructor(){
+        super();
+        this.i = 0;
+        this.span = document.createElement('span')
+        this.span.classList.add('badge', 'text-bg-secondary');
+        this.span.innerHTML = this.i;
+        this.appendChild(this.span)
+        window.setInterval(()=>{
+            this.i++;
+            this.span.innerHTML = this.i;
+        }, 1000)
+        //this.innerHTML = 'Mon compteur';
+
+    }
+}
+
+customElements.define('second-timer', SecongTimer);
